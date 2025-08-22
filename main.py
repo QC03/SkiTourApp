a4b39ea7e1a63270b35962a11fbe8024a47d0501
QC03 <tjdwns5869@naver.com>
2025-08-22 11:36:14 +0900

    Rename: Create Folder and Rename UI Files

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow, QTabWidget
 import db
 
-from ui_instructor import InstructorTab
-from ui_booking import BookingTab
-from ui_schedule import ScheduleTab
-from ui_report import ReportTab
-from ui_reservation import ReservationTab
+from ui.instructor import InstructorTab
+from ui.booking import BookingTab
+from ui.schedule import ScheduleTab
+from ui.report import ReportTab
+from ui.reservation import ReservationTab
 
 class MainWindow(QMainWindow):
     def __init__(self):
